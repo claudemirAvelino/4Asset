a0a166a0b849cfc315852d0a523cecccdb504ce7
Junior Avelino <junior.a98@hotmail.com>
2023-05-17 00:46:41 -0300

    feat: configuração do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserInterfaceComponent} from "./pages/user-interface/user-interface.component";
+import {UserFormComponent} from "./components/user-form/user-form.component";
+import {PopupComponent} from "./components/popup/popup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserInterfaceComponent
+  },
+  {
+    path: 'form',
+    component: PopupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxMaskModule } from 'ngx-mask'
+import { UserInterfaceComponent } from './pages/user-interface/user-interface.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInterfaceComponent,
+    HeaderComponent,
+    UserFormComponent,
+    PopupComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
